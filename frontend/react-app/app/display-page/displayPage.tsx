40100475db4223f37ceac0a70a81477170311472
nosra <77589065+nosra@users.noreply.github.com>
2025-02-19 23:34:38 -0500

    added React + React Router scaffolding

---
+++ frontend/react-app/app/display-page/displayPage.tsx
@@ -0,0 +1,16 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function DisplayPage() {
+  return (
+    <>
+      <main className="display-page">
+        <div className="display-page-header">
+          <h1>DegenAI</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+
