13dbbe52222fc227a89bdac84f10661ace0da492
Alon <a.leshem20@gmail.com>
2025-03-06 19:01:13 -0500

    adds equality and hashing for determining uniqueness

--- backend/Coin.py
+++ backend/Coin.py
@@ -6,3 +6,11 @@ class Coin:
 
     def __str__(self):
         return f"{self.name} ({self.symbol}): {self.price}"
+
+    # equality
+    def __eq__(self, other):
+        return self.symbol == other.symbol
+
+    # hash
+    def __hash__(self):
+        return hash(self.symbol)
