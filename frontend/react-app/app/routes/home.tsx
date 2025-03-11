30218495c8a4a69c29f3336b4e9bb6ddba7c0afc
nosra <77589065+nosra@users.noreply.github.com>
2025-03-11 17:50:16 -0400

    updated home.tsx

--- frontend/react-app/app/routes/home.tsx
+++ frontend/react-app/app/routes/home.tsx
@@ -3,7 +3,7 @@ import HomePage from "../home-page/HomePage";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "DegenAI Home Page" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
