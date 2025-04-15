3d280d045d5cfd087a2b0c908f8ea68bfacb1a0b
Alon <a.leshem20@gmail.com>
2025-04-15 16:17:59 -0400

    adds coin sentiment analysis

--- backend/SentimentAPI.py
+++ backend/SentimentAPI.py
@@ -7,16 +7,6 @@ load_dotenv()
 GPT_BASE_URL = 'https://api.openai.com'
 GPT_API_KEY = os.getenv('GPT_API_KEY')
 
-# Authorization: Bearer OPENAI_API_KEY
-# curl https://api.openai.com/v1/chat/completions \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: Bearer $OPENAI_API_KEY" \
-#   -d '{
-#      "model": "gpt-4o-mini",
-#      "messages": [{"role": "user", "content": "Say this is a test!"}],
-#      "temperature": 0.7
-#    }'
-
 class SentimentAPIError(APIError):
   def __init__(self, message):
     self.message = message
@@ -31,7 +21,7 @@ class SentimentAPI(AbstractAPI):
     "Content-Type": "application/json",
   }
 
-  def create_data(self, content, model="", role="", temp=0):
+  def _create_data(self, content, model="", role="", temp=0):
     if not model:
       model = self.model
     if not role:
@@ -45,14 +35,14 @@ class SentimentAPI(AbstractAPI):
       "temperature": temp
     }
 
-  def __init__(self, model="gpt-4o-mini", role="user", temp=0.7):
+  def __init__(self, model="gpt-4o", role="user", temp=0.7):
     super().__init__(GPT_BASE_URL, GPT_API_KEY)
     self.model = model
     self.role = role
     self.temp = temp
 
-  def test(self):
-    test_data = self.create_data("Say this is a test!")
+  def _test(self):
+    test_data = self._create_data("Say this is a test!")
     print(test_data)
     input()
 
@@ -63,10 +53,36 @@ class SentimentAPI(AbstractAPI):
     if response.status_code != 200:
       raise SentimentAPIError(f"API returned status code {response.status_code}.\n{response.text}")
 
-    return response.json()
+    return response.json()['choices'][0]['message']['content']
 
+    
+
+  def _get_response(self, content):
+    data = self._create_data(content)
+    response = self.post(endpoint='/v1/chat/completions', 
+                    data=data,
+                    headers=self.header)
+
+    if response.status_code != 200:
+      raise SentimentAPIError(f"API returned status code {response.status_code}.\n{response.text}")
+
+    return response.json()['choices'][0]['message']['content']
+
+  def get_coin_sentiment(self, coin):
+    coin_name = coin.name
+    coin_symbol = coin.symbol
+    coin_url = f"https://pump.fun/coin/{coin.url}"
+
+    message = f"Take a look at the cryptocurrency {coin_name} ({coin_symbol}) from {coin_url}. Rate it out of 10 based on its social media presence. Just provide the rating with no other text."
+
+    return self._get_response(message)
 # test
 if __name__ == '__main__':
-  sentimentAPI = SentimentAPI()
-  response = sentimentAPI.test()
+  sentimentAPI = SentimentAPI(model='gpt-4o')
+  import CryptoAPI
+  cryptoAPI = CryptoAPI.CryptoAPI()
+  clist = cryptoAPI.get_coin_list()
+  coin = clist[0]
+  print(coin)
+  response = sentimentAPI.get_coin_sentiment(coin)
   print(response)
